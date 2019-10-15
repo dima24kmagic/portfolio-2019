93551907b97e59629b2153e90601e67bc8897a16
Dima Baranov <vajnoe-dima@mail.ru>
2019-10-15 19:33:58 +0300

    feat(scroll-bar): Calculating scrollbar position right way

--- src/components/SmoothScroll/hooks/useScrollBarStyles.ts
+++ src/components/SmoothScroll/hooks/useScrollBarStyles.ts
@@ -1,28 +1,35 @@
 import { MutableRefObject, useEffect, useState } from 'react'
 import { useSpring } from 'react-spring'
 
-const SCROLLO_FFSET = 16
+const SCROLL_OFFSET = 16
 
 const useScrollBarStyles = (
   currentScrollDeltaY: number,
   scrollWrapperRef: MutableRefObject<HTMLDivElement>,
 ) => {
+  let wrapperHeight = 1
+  if (scrollWrapperRef.current) {
+    wrapperHeight = scrollWrapperRef.current.clientHeight
+  }
+
   const [scrollHeight, setScrollHeight] = useState(0)
   useEffect(() => {
     if (scrollWrapperRef.current) {
+      wrapperHeight = scrollWrapperRef.current.clientHeight
       setScrollHeight(
         window.innerHeight *
           (window.innerHeight / scrollWrapperRef.current.clientHeight) -
-          SCROLLO_FFSET * 2,
+          SCROLL_OFFSET * 2,
       )
     }
   }, [scrollWrapperRef])
 
   const handleWindowResize = () => {
+    wrapperHeight = scrollWrapperRef.current.clientHeight
     setScrollHeight(
       window.innerHeight *
         (window.innerHeight / scrollWrapperRef.current.clientHeight) -
-        SCROLLO_FFSET * 2,
+        SCROLL_OFFSET * 2,
     )
   }
   useEffect(() => {
@@ -31,11 +38,13 @@ const useScrollBarStyles = (
   }, [])
 
   // 100%
-  const scrollPercentage = Math.abs(currentScrollDeltaY) + window.innerHeight
+  const scrollPercentage = wrapperHeight
   // Percents to move by depends on window
   const movePercentage =
     (currentScrollDeltaY / scrollPercentage) * window.innerHeight -
-    SCROLLO_FFSET
+    SCROLL_OFFSET
+
+  console.log({ movePercentage, scrollPercentage })
 
   const [scrollStyles, setScrollStyles] = useSpring(() => ({
     transform: `translate3d(0px, ${movePercentage * -1}px, 0px)`,
