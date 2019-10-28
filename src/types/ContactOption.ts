0c1a1a41b002d5f29181a2c65510f96e9ebeb8de
Dima Baranov <vajnoe-dima@mail.ru>
2019-10-28 15:00:38 +0300

    feat(contact-option): Add type

---
+++ src/types/ContactOption.ts
@@ -0,0 +1,11 @@
+interface ContactOptionType {
+  name: string
+  href: string
+  type: string
+  copyPlaceholder: string
+  value: string
+  rel?: string
+  target?: string
+}
+
+export default ContactOptionType
