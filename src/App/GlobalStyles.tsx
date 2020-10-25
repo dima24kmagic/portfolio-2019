d624bb254f5ce54f24f360365425e9ab4447df1b
Dzmitry <vajnoe-dima@mai.ru>
2020-10-25 12:00:53 +0300

    feat(css-imports): removed css @import from global styles

--- src/App/GlobalStyles.tsx
+++ src/App/GlobalStyles.tsx
@@ -3,9 +3,6 @@ import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = () => {
   const Inject = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap');
-  @import url('https://fonts.googleapis.com/css?family=Chivo:300,700,900&display=swap');
-  @import url('https://fonts.googleapis.com/css2?family=Work+Sans:wght@100;200;300;400;600;700;900&display=swap');
   *{
     margin: 0;
     padding: 0;
