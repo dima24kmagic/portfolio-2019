2efbd5cc093e3f3029762d21d68cd7010ffe6fa1
Dzmitry <vajnoe-dima@mai.ru>
2020-10-26 12:11:55 +0300

    feat(removed-transform): transform removed because was eating resources

--- src/App/GlobalStyles.tsx
+++ src/App/GlobalStyles.tsx
@@ -12,7 +12,6 @@ const GlobalStyle = () => {
     backface-visibility: hidden;
     -webkit-overflow-scrolling: touch;
     user-select: text !important;
-    transform: translate3d(0,0,0);
 }
   html, 
   body {
