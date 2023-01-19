12879395f0a5978f96c329a1d0c83805cdfaa789
Dzmitry Baranau <dzmitrybaranau@Dzmitrys-MBP-14.Home>
2023-01-19 13:21:23 +0000

    feat(ContactModal): animations for copying

--- components/ContactModal/components/ContactOption.tsx
+++ components/ContactModal/components/ContactOption.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import styled, { css } from "styled-components";
 import {
   Button,
@@ -6,6 +6,7 @@ import {
   TypographyWeight,
 } from "../../StyledComponents";
 import { theme } from "../../../theme/theme";
+import { AnimatePresence, motion } from "framer-motion";
 
 export interface IContactOptionProps {
   label: string;
@@ -21,10 +22,28 @@ const Root = styled.div`
   flex-direction: column;
   align-items: flex-start;
   margin-bottom: 18px;
+  overflow: hidden;
 
   ${theme.breakpoints.md} {
     align-items: center;
   }
+`;
+
+const CopiedOverlayAnimation = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  background: #000;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+const CopiedTextAnimated = styled(motion.p)`
+  color: white;
+  position: absolute;
+  top: 0;
+  left: 50%;
+  transform: translate(-50%, -50%);
 `;
 
 const ValueToCopy = styled.textarea`
@@ -48,8 +67,10 @@ const ContactOption = ({
   copyLabel,
   copyValue,
 }: IContactOptionProps) => {
+  const [isCopied, setIsCopied] = useState(false);
   const textAreaRef = useRef<HTMLTextAreaElement>();
   const handleCopy = () => {
+    setIsCopied(true);
     textAreaRef?.current?.select();
     document.execCommand("copy");
     textAreaRef?.current?.blur();
@@ -82,10 +103,44 @@ const ContactOption = ({
         onClick={handleCopy}
         pure
         customStyle={css`
+          position: relative;
+          overflow: hidden;
+          border-radius: 0px;
+          height: 24px;
           &:focus {
+            outline: none;
           }
         `}
       >
+        <AnimatePresence>
+          {isCopied && (
+            <>
+              <CopiedOverlayAnimation
+                initial={{ x: "-100%" }}
+                animate={{ x: 0, transition: { duration: 0.5 } }}
+                exit={{ x: "100%", transition: { delay: 0.85, duration: 0.5 } }}
+                onAnimationComplete={() => {
+                  setIsCopied(false);
+                }}
+              />
+              <CopiedTextAnimated
+                initial={{ top: "-20%", opacity: 0 }}
+                animate={{
+                  top: "50%",
+                  opacity: 1,
+                  transition: { duration: 0.5, delay: 0 },
+                }}
+                exit={{
+                  top: "100%",
+                  opacity: 0,
+                  transition: { delay: 0.75, duration: 0.5 },
+                }}
+              >
+                Copied!
+              </CopiedTextAnimated>
+            </>
+          )}
+        </AnimatePresence>
         <GradientTypography
           customStyles={css`
             background: ${gradient};
