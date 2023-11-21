8a7d4ec5231919e902d5239541d8ca4fdec50b1e
Dzmitry Baranau <dimabaranov98@gmail.com>
2023-11-21 15:30:38 +0300

    feat(CV): pdf update

--- components/Sections/components/ContactDownloadButtons.tsx
+++ components/Sections/components/ContactDownloadButtons.tsx
@@ -40,7 +40,7 @@ function ContactDownloadButtons(props: IInitiativeButtonsProps) {
   const { onContactMeClick, isModalOpen } = props;
   const handleDownload = () => {
     // Get the URL of the PDF file
-    const pdfUrl = "/files/CV - Dzmitry Baranau.pdf";
+    const pdfUrl = "/files/Dzmitry Baranau - CV.pdf";
 
     // Create a temporary anchor element
     const anchor = document.createElement("a");
